2ce3689e8f33e030a0abb6e15eb4ea7fbf2614c4
antoan-angelov <antoan.angelov@gmail.com>
2015-01-13 01:45:17 +0200

    Optimized Parenthesis.java

--- src/objects/Parenthesis.java
+++ src/objects/Parenthesis.java
@@ -2,11 +2,10 @@ package objects;
 
 public class Parenthesis extends MathObject {
     
-    public final static Parenthesis LEFT_PARENTHESIS = new Parenthesis(TYPE_PARENTHESIS_LEFT);
-    public final static Parenthesis RIGHT_PARENTHESIS = new Parenthesis(TYPE_PARENTHESIS_RIGHT);
+    public final static Parenthesis LEFT_PARENTHESIS = new Parenthesis("(", TYPE_PARENTHESIS_LEFT);
+    public final static Parenthesis RIGHT_PARENTHESIS = new Parenthesis(")", TYPE_PARENTHESIS_RIGHT);
         
-    private Parenthesis(int type) {
-        super(type == TYPE_PARENTHESIS_LEFT 
-                ? "(" : type == TYPE_PARENTHESIS_RIGHT ? ")" : "", type);
+    private Parenthesis(String name, int type) {
+        super(name, type);
     }
 }
