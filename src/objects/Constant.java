9bc9afb083fdc9b0acedd9c8a3a119d2e5afd21d
antoan-angelov <antoan.angelov@gmail.com>
2015-01-13 23:52:58 +0200

    Fixed PI value

--- src/objects/Constant.java
+++ src/objects/Constant.java
@@ -5,7 +5,7 @@ public class Constant extends Number {
 
     private final static ArrayList<Constant> CONSTANTS = new ArrayList<>();
     
-    public final static Constant CONSTANT_PI = new Constant("pi", 3.14);
+    public final static Constant CONSTANT_PI = new Constant("pi", 3.14159265359);
     public final static Constant CONSTANT_E = new Constant("e", 2.71828);
     
     private Constant(String name, double value) {
